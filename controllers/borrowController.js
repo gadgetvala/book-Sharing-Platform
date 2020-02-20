285bdc16ecee0d8b6d8651e085e3d9f5510a76d0
saikiran rudra <saikiranrudra2@gmail.com>
2020-02-20 17:36:32 +0530

    borrow query made more efficient

--- controllers/borrowController.js
+++ controllers/borrowController.js
@@ -4,25 +4,44 @@ const User = require('./../models/userModel.js');
 
 exports.getAll = async (req, res) => {
 	try {
-		const borrow = await Borrow.find();
+		// const borrow = await Borrow.find();
 		
-		let borrowerIds=[];
-		let booksIds=[];
-		borrow.forEach(borrow => {
-			booksIds.push(borrow.bookID);
-			borrowerIds.push(borrow.userID);
-		});
+		// let borrowerIds=[];
+		// let booksIds=[];
+		// borrow.forEach(borrow => {
+		// 	booksIds.push(borrow.bookID);
+		// 	borrowerIds.push(borrow.userID);
+		// });
 
-		const borrowers = await User.find({ _id: {$in: borrowerIds} });
-		const books = await Book.find({ _id: {$in: booksIds} });	
+		// const borrowers = await User.find({ _id: {$in: borrowerIds} });
+		// const books = await Book.find({ _id: {$in: booksIds} });	
 
-		const result = borrowers.map((borrow, index) => {
-			return ({
-							user: borrow,
-							book: books[index]
-						})
-		})
+		// const result = borrowers.map((borrow, index) => {
+		// 	return ({
+		// 					user: borrow,
+		// 					book: books[index]
+		// 				})
+		// })
 
+		const result = await Borrow.aggregate([
+			{
+				$lookup: {
+					from: "users",
+					localField: "userID",
+					foreignField: "_id",
+					as: "userInfo"
+
+				}	
+			},
+			{
+				$lookup: {
+					from: "books",
+					localField: "bookID",
+					foreignField: "_id",
+					as: "bookData"
+				}
+			}
+		])
 
 		res.status(200).json({
 			status: 'success',
@@ -37,6 +56,7 @@ exports.getAll = async (req, res) => {
 			data: { message: err.message }
 		});
 	}
+	
 };
 
 exports.getAllByUser = async (req, res) => {
